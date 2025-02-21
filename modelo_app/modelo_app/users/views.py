a1e110ae7b20eb96bd6476ebeba9dd2910cd428c
Scarkat <119711013+Scarkat@users.noreply.github.com>
2025-02-21 17:47:23 -0600

    Arreglos

--- modelo_app/modelo_app/users/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import User
-
-def usersIndex(request):
-    users = User.objects.all()
-    return render(request, "users/index.html", {"users":users})
